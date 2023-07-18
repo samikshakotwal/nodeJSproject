bdfc3fcce67b9faf0adbcc818ca555934b15395b
samikshakotwal <samikshakotwal1422@gmail.com>
2023-07-18 10:33:23 +0530

    sami

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var app = require('../index.js');describe('GET /', function() {
 
  it('respond with hello world', function(done) { //navigate to root and check the the response is "hello world"
 
- request(app).get('/').expect('samiksha', done);
+ request(app).get('/').expect('samiksha manoj', done);
 
  });
 
