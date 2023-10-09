9dadcbaedc5111a5a70488d7179f50227ea73292
samikshakotwal <samikshakotwal1422@gmail.com>
2023-10-09 11:04:31 +0530

    asdfgh

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-///importing node framework
 
-var express = require(‘express’);
-
-
-var app = express();
-
+//importing node framework
+varexpress=require("express");
+ 
+varapp=express();
 //Respond with "hello world" for requests that hit our root "/"
-
-app.get(‘/’, function (req, res) {
-
-res.send(‘samiksha manoj’);
-
-});
-
+app.get("/", function (req, res) {​​​​​​​​
+res.send("hello world");
+}​​​​​​​​);
 //listen to port 80 by default
-
-app.listen(process.env.PORT || 80);
-
-
-module.exports = app;
-
-
-execute shell- npm inst
+app.listen(process.env.PORT||80);
+ 
+module.exports=app;
 
 
 
